d140c3c7ad0775b71562c75a47bfcdc8d428ac8f
Roma <romagupta701@gmail.com>
2023-07-07 11:41:02 +0530

    product folder

---
+++ WebApp/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,15 @@
+using CoreLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Products
+{
+    public class CreateModel : PageModel
+    {
+      //  private ProductService productService; 
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApp/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using CoreLogic.Models;
+using CoreLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products { get; set; }
+
+        public void OnGetAsync()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetAllProducts();
+        }
+    }
+}
