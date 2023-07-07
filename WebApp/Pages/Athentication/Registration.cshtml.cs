bfa4372612cfc55c98843987277d53fec5e8a8c1
Indresh <indresh.nitjsr@gmail.com>
2023-07-07 15:54:50 +0530

    athentication

---
+++ WebApp/Pages/Athentication/Registration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Athentication
+{
+    public class RegistrationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
